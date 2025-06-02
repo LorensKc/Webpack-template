608892f847879274fdf0dfa9854bd0d65807c51e
Makc <tvinkiman@gmail.com>
2025-06-02 14:47:47 +0300

    fixed builder

---
+++ dist/js/bundle.js
@@ -0,0 +1,87 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/scss/main.scss":
+/*!****************************!*\
+  !*** ./src/scss/main.scss ***!
+  \****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry needs to be wrapped in an IIFE because it needs to be isolated against other modules in the chunk.
+(() => {
+/*!*************************!*\
+  !*** ./src/js/index.js ***!
+  \*************************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _scss_main_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../scss/main.scss */ "./src/scss/main.scss");
+// Імпортуємо головний SCSS файл, щоб Webpack його обробив
+
+
+// Приклад простого JS
+console.log("JavaScript завантажено!");
+document.addEventListener("DOMContentLoaded", function () {
+  var button = document.getElementById("myButton");
+  if (button) {
+    button.addEventListener("click", function () {
+      alert("Кнопку натиснуто!");
+    });
+  }
+});
+
+// Приклад використання сучасного синтаксису (для Babel)
+var greet = function greet() {
+  var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : "Світ";
+  console.log("\u041F\u0440\u0438\u0432\u0456\u0442, ".concat(name, "!"));
+};
+greet();
+})();
+
+/******/ })()
+;
--- webpack.config.js
+++ webpack.config.js
@@ -6,25 +6,14 @@ const isProduction = process.env.NODE_ENV === "production";
 
 module.exports = {
   mode: isProduction ? "production" : "development",
-  entry: "./src/js/index.js", // Головний JS файл
+  entry: "./src/js/index.js",
   output: {
-    filename: "js/bundle.js", // Назва вихідного JS файлу
-    path: path.resolve(__dirname, "dist"), // Папка для збірки
-    clean: true, // Очищати папку dist перед кожною збіркою (Webpack 5+)
-    //publicPath: isProduction ? "./" : "/", // Важливо для devServer та шляхів до ресурсів
+    filename: "js/bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
     publicPath: "./",
   },
-  devtool: isProduction ? "source-map" : "eval-source-map", // Source maps для дебагу
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "dist"), // Звідки сервірувати файли
-    },
-    compress: true,
-    port: 9000,
-    open: true, // Автоматично відкривати браузер
-    hot: true, // Hot Module Replacement
-    watchFiles: ["src/**/*.html", "src/**/*.scss", "src/**/*.js"], // Стежити за змінами
-  },
+  devtool: false,
   module: {
     rules: [
       {
@@ -40,88 +29,54 @@ module.exports = {
       {
         test: /\.(scss|css)$/,
         use: [
-          MiniCssExtractPlugin.loader, // 3. Витягує CSS в окремий файл
-          "css-loader", // 2. Інтерпретує @import, url()
+          MiniCssExtractPlugin.loader,
           {
-            loader: "postcss-loader", // 1.5 (опціонально) Обробляє CSS за допомогою PostCSS
+            loader: "css-loader",
             options: {
-              postcssOptions: {
-                config: path.resolve(__dirname, "postcss.config.js"),
+              sourceMap: true,
+            },
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true,
+              implementation: require("sass"),
+              sassOptions: {
+                outputStyle: "expanded",
               },
             },
           },
-          "sass-loader", // 1. Компілює Sass/SCSS в CSS
         ],
       },
       {
-        test: /\.html$/, // Для обробки HTML файлів, якщо потрібно (наприклад, для image src)
-        use: ["html-loader"], // Може бути корисним для завантаження ресурсів, на які посилається HTML
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: {
+              sources: false,
+              minimize: false,
+            },
+          },
+        ],
       },
-      // Можна додати завантажувачі для зображень, шрифтів
-      // {
-      //   test: /\.(png|svg|jpg|jpeg|gif)$/i,
-      //   type: 'asset/resource',
-      //   generator: {
-      //     filename: 'images/[hash][ext][query]'
-      //   }
-      // },
-      // {
-      //   test: /\.(woff|woff2|eot|ttf|otf)$/i,
-      //   type: 'asset/resource',
-      //   generator: {
-      //     filename: 'fonts/[hash][ext][query]'
-      //   }
-      // },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html", // Шлях HTML шаблону
-      filename: "index.html", // Назва вихідного HTML файлу
-      inject: "body", // Куди вставляти скрипти ('body', 'head', true, false)
-      minify: false, // Не мініфікувати
-      // minify: isProduction ? {
-      //       removeComments: false, // Зберігати коментарі
-      //       collapseWhitespace: false, // Не згортати пробіли
-      //       removeAttributeQuotes: false, // Не видаляти лапки з атрибутів
-      //     } : false, // Не мініфікувати для development
+      template: "./src/index.html",
+      filename: "index.html",
+      inject: "body",
+      minify: false,
     }),
     new MiniCssExtractPlugin({
-      filename: "css/style.css", // Назва вихідного CSS файлу
+      filename: "css/style.css",
     }),
   ],
   optimization: {
-    minimize: isProduction ? true : false, // Вмикання-Вимикання
-    // minimize: false, // Вимикання
-    minimizer: [
-      (compiler) => {
-        const TersePlugin = require("terser-webpack-plugin");
-        new TersePlugin({
-          terserOptions: {
-            compress: {
-              drop_console: isProduction, // Вимикає console.log
-            },
-            mangle: isProduction, // Плутає назви змінних
-            format: {
-              comments: !isProduction, // Зберігання коментарів
-              beautify: !isProduction, // Читбельний JS
-            },
-          },
-          extractComments: false, // Не витягувати коменти в окремий файл
-        }).apply(compiler);
-      },
-      // Для CSS
-      // const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-      // new CssMinimizerPlugin({
-      //   minimizerOptions: {
-      //     preset: [
-      //       "default",
-      //       {
-      //         discardComments: { removeAll: !isProduction },
-      //       },
-      //     ],
-      //   },
-      // }),
-    ],
+    minimize: false,
+  },
+  performance: {
+    hints: false,
   },
 };
