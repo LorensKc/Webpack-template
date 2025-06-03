49b8c638e12c76561af87bf17ae76c1993093aa3
Makc <tvinkiman@gmail.com>
2025-06-03 09:02:26 +0300

    last fix

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  plugins: [
-    require("postcss-preset-env")({
-      // Тут можна налаштувати, які фічі підтримувати
-      // Наприклад, для підтримки останніх 2 версій браузерів:
-      // browsers: 'last 2 versions',
-    }),
-  ],
-};
